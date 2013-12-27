ae8c2bca6b602f543a36c09ae28112a606384d65
U-SJC_PC\SJC <redsox333@gmail.com>
2013-12-26 20:13:53 -0800

    Fixing static file serving

--- src/node/app.js
+++ src/node/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 module.exports = function() {
 	var app = express();
-	app.use(express.static(__dirname + '/app'));
+	app.use(express.static(__dirname + '/../app'));
 	app.use(express.cookieParser());
 	app.use(express.session({ secret: "keyboard cat" }));
 	app.use(express.bodyParser());
